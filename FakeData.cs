8ca032d2d121351df045036ee371a125c535d6c4
aslanonurcan <aslan.onurcan@yandex.com.tr>
2019-02-23 13:39:58 +0300

    first

---
+++ FakeData.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SahteVeri
+{
+    
+    class FakeData
+    {
+        public int id { get; set; }
+        public string isim { get; set; }
+
+        public string soyİsim { get; set; }
+        public int yas { get; set; }
+        public int number { get; set; }
+
+        Random rnd = new Random();
+        public static string[] isimlst = { "Onur" , "Serkan" , "Yusuf" , "Nükhet"  , "Selcen" , "Hasan" , "Ahmet" , "Günışığı" , "Gülşah" ,"Gülcan" };
+        public static string[] soyİsimlst = { "Atan", "Tutan", "Bakan", "Koç", "Sabancı", "Gençoğlu" , "Karaca" , "Manço" , "Koray" , "Kenter"};
+        public static int[] yaslst = new int[10];
+        public static int[] numberlst = new int[10];
+
+         
+        //public void _number()
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        numberlst[i] = rnd.Next(0, 100);
+        //    }
+
+        //}
+
+        //public void _yas()
+        //{
+
+
+    //}
+
+
+    public static object Karma()
+        {
+            List<FakeData> olist = new List<FakeData>();
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            { 
+                numberlst[i] = rnd.Next(0, 100);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                yaslst[i] = rnd.Next(18, 30);
+            }
+
+            for (int i = 1; i <= 100; i++)
+            {
+                FakeData o = new FakeData();
+                string x, y;
+                o.id = i;
+                x = isimlst[rnd.Next(0, 10)];
+                o.isim = x;
+                y = soyİsimlst[rnd.Next(0, 10)];
+                o.soyİsim = y;
+                o.yas = yaslst[rnd.Next(0, 10)];
+                o.number= numberlst[rnd.Next(0, 10)];
+                olist.Add(o);
+            }
+
+            return olist;
+        }
+
+        //public static string Karmaisim(int i)
+        //{
+
+        //    string x;
+        //    x = isimlst[i];
+        //    return x;
+        //}   
+
+        //public static int Karmayas(int i)
+        //{
+            
+        //    int x;
+        //    x = yaslst[i];
+        //    return x;
+        //}
+
+        //public static int Karmanumber(int i)
+        //{
+            
+        //    int x;
+        //    x = numberlst[i];
+        //    return x;
+        //}
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SahteVeri
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        List<FakeData> blist = new List<FakeData>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Doldur();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //for (int i = 1; i <= 100; i++)
+            //{
+
+            //    FakeData o1 = new FakeData();
+                
+
+
+            //}
+
+
+
+        }
+
+        private void Doldur()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = FakeData.Karma();
+
+            
+        }
+
+    }
+}
